3cb5a8aa24c28428cd708d981463800152d95eff
sathishp3190 <sathishp3190@gmail.com>
2019-08-15 12:22:06 +0530

    second Commit

--- main/java/org/cts/test/CoreJava/BrowserLaunch.java
+++ main/java/org/cts/test/CoreJava/BrowserLaunch.java
@@ -1,6 +1,8 @@
 package org.cts.test.CoreJava;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserLaunch {
@@ -8,5 +10,8 @@ public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shylaja\\eclipse-workspace\\CoreJava\\Driver\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.seleniumhq.org/download/");
+	WebElement text = driver.findElement(By.xpath("//a[text()='Previous Releases']"));
+	text.click();
+	
 }
 }
